2ed3dee60ec0f38e660d04dbe79e38e3356f08cf
Screechie <5440543+Screechie@users.noreply.github.com>
2019-03-08 19:46:08 -0400

    Added code for mystery2 and mystery3

--- mystery.cpp
+++ mystery.cpp
@@ -14,7 +14,7 @@ void mystery1(auto& Data)
 {
   cout<<endl<<"Mystery 1"<<endl<<"---------------------"<<endl;
 
-  for ( int i = 0 ; i < Data.size( ) ; i++)
+  for ( int i = 0 ; i < Data.size() ; i++)
   {
     for ( int j = 0 ; j < i ; j++)
 	if ( Data[ i ] < Data[ j ] )
@@ -26,6 +26,30 @@ void mystery1(auto& Data)
 }
 
 //... Other mysteries...
+//mystery2
+void mystery2(auto &Data){
+	cout<<endl<<"Mystery 2"<<endl<<"---------------------"<<endl;
+	for(int i = 0; i < Data.size(); i++) {
+	  for(int j=0; j < Data.size() - 1; j++) {
+		if(Data[j+1] < Data[j])
+			swap(Data[j+1], Data[j]);
+	  }
+	 print(Data);
+	}
+	
+}
+
+//mystery3
+void mystery3(auto &Data){
+	cout<<endl<<"Mystery 3"<<endl<<"---------------------"<<endl;
+	for(int i = 0; i < Data.size(); i++) {
+	  for(int j=i; j < Data.size(); j++) {
+		if(Data[j] < Data[i])
+			swap(Data[i], Data[j]);
+	  }
+	print(Data);  
+	}
+}
 
 int main()
 {
